c29c40631c031e489d8641ffe694a825f1d3479c
Shay.Lami <Shay@lami.co.il>
2017-02-13 17:07:19 +0200

    MR4

--- Delivers_CRM/Pages/Home.aspx.designer.cs
+++ Delivers_CRM/Pages/Home.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace Delivers_CRM.Pages {
         protected global::System.Web.UI.WebControls.GridView GvJornal;
         
         /// <summary>
+        /// SqlDataSourceBikesDelivers control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSourceBikesDelivers;
+        
+        /// <summary>
         /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
